97dfc998ff9dff7aa3074debf285a0c0c4786768
Jesus Salinas <jesussalinas@MacBook-Pro-de-Clara.local>
2021-12-19 10:07:24 -0600

    adding commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,57 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const faker = require('faker');
+faker.locale = 'es_MX';
+
+Cypress.Commands.add('get_CLABE', () => {
+    cy.fixture('catalogue/catalogue.json').then((cat) => {
+        var bankCode = cat.bank_code[Math.floor(Math.random() * cat.bank_code.length)].code;   
+        var account = faker.finance.mask(11);
+        var clabe = bankCode + cat.place_code[Math.floor(Math.random() * cat.place_code.length)].code + account + '1';
+        return clabe;
+    });
+});
+
+Cypress.Commands.add('get_RFC', () => {
+    var rfc;
+    var verify;
+    var sum = 0;
+    const name = faker.name.firstName().slice(0,1).toUpperCase();
+    var lastName = faker.name.lastName().slice(0,2);
+    lastName = lastName.replace(/á/gi,'a');
+    lastName = lastName.replace(/é/gi,'e');
+    lastName = lastName.replace(/í/gi,'i');
+    lastName = lastName.replace(/ó/gi,'o');
+    lastName = lastName.replace(/ú/gi,'u');
+    lastName = lastName.replace(/ñ/gi,'n');
+    lastName = lastName.toUpperCase();
+    const middleName = faker.name.middleName().toUpperCase();
+    const date = faker.date.past(50,'2010-01-01').toLocaleDateString('es-MX', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit'
+    });
+    const day = date.slice(0,2);
+    const month = date.slice(3,5);
+    const year = date.slice(6,8);
+    var homonimio = faker.name.lastName().slice(0,2);
+    homonimio = homonimio.replace(/á/gi,'a');
+    homonimio = homonimio.replace(/é/gi,'e');
+    homonimio = homonimio.replace(/í/gi,'i');
+    homonimio = homonimio.replace(/ó/gi,'o');
+    homonimio = homonimio.replace(/ú/gi,'u');
+    homonimio = homonimio.replace(/ñ/gi,'n');
+    homonimio = homonimio.toUpperCase();
+    rfc = lastName + middleName + name + year + month + day + homonimio;
+    const dictionary = '0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ Ñ';
+    for(var i=0; i<12; i++) {
+        sum += dictionary.indexOf(rfc.charAt(i)) * (13 - i); 
+    }
+    verify = 11 - sum % 11; 
+    if(verify == 11) {
+        verify = 0; 
+    }  
+    else if(verify == 10) {
+        verify = 'A';
+    } 
+    rfc = rfc + verify;
+    return rfc; 
+});
